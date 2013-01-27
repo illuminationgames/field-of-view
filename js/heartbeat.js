70ac4d61c9b10f91a16808f6b08cc7990c7653f1
Danielle Church <danielle@illuminationgames.com>
2013-01-26 20:34:22 -0500

    changing homing beacon again, also adding disable/blank screen to overlay

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -25,10 +25,10 @@ function HeartbeatCanvas() {
 	this.lastFrame = false;
 	this._aFrameProxy = $.proxy(this, '_animFrame');
 	this.beatSpeed = 200;
-	this.beatMax = 250;
 	this.targetVisibility = false;
 	this.visSpeed = false;
-	this._setVisibility(this.beatMax);
+	this.visibility = false;
+	//this._setVisibility(this.beatMax);
 	this.lastClone = -1;
 	this._homeDirection = 0;
 	lowLag.init({'urlPrefix':'audio/','debug':'none'});
@@ -52,32 +52,51 @@ $.extend(HeartbeatCanvas.prototype, {
 			top: y - 600
 		});
 	},
+	disable: function() {
+		this.setPulse(false);
+		this.beats = [];
+		this._setVisibility(false);
+	},
 	setVisibility: function(targetVis) {
-		this.targetVis = targetVis;
-		this.visSpeed = (targetVis - this.visibility) * 3;
+		if (this.pulse) {
+			this.targetVis = targetVis;
+			this.visSpeed = (targetVis - this.visibility) * 3;
+		} else {
+			this._setVisibility(targetVis);
+		}
 	},
 	setHomeDirection: function(x, y) {
 		this._homeDirection = Math.atan2(y, x);
 	},
 	_setVisibility: function(maxVis) {
-		this.visibility = this.beatMax = maxVis;
-		var maxRad = maxVis + 20;
-		var gradDef = 'circle '+maxRad+'px, rgba(0,0,0,0) 0px';
-		if (maxVis > 70)
-			gradDef += ', rgba(0,0,0,0.2) 50px';
-		//console.log(maxRad,maxVis,beatRad);
-		/*
-		if (beatRad >= maxVis - 40)
-			beatRad = maxVis - 40;
-		gradDef += ', rgba(0,0,0,0.3) '+(beatRad)+'px';
-		*/
-		gradDef += ', rgba(0,0,0,0.5) '+(maxVis-20)+'px';
-		gradDef += ', rgba(0,0,0,0.8) '+maxRad+'px';
-		//console.log('visdef',gradDef);
-		this.backdrop.css({
-			backgroundImage: 'radial-gradient('+gradDef+')'
-		});
-		this.setPulse(this._pulseFromVisibility(maxVis));
+		this.visibility = maxVis;
+		if (maxVis === false) {
+			this.backdrop.css('background','');
+		} else if (maxVis > 20) {
+			var maxRad = maxVis + 20;
+			var gradDef = 'circle '+maxRad+'px, rgba(0,0,0,0) 0px';
+			if (maxVis > 70)
+				gradDef += ', rgba(0,0,0,0.2) 50px';
+			//console.log(maxRad,maxVis,beatRad);
+			/*
+			if (beatRad >= maxVis - 40)
+				beatRad = maxVis - 40;
+			gradDef += ', rgba(0,0,0,0.3) '+(beatRad)+'px';
+			*/
+			gradDef += ', rgba(0,0,0,0.5) '+(maxVis-20)+'px';
+			gradDef += ', rgba(0,0,0,0.8) '+maxRad+'px';
+			//console.log('visdef',gradDef);
+			this.backdrop.css({
+				backgroundImage: 'radial-gradient('+gradDef+')'
+			});
+			this.setPulse(this._pulseFromVisibility(maxVis));
+		} else {
+			this.backdrop.css({
+				background: 'black'
+			});
+			this.visibility = 0;
+			this.setPulse(false);
+		}
 	},
 	_pulseFromVisibility: function(maxVis) {
 		var highPulse=160, lowPulse=60;
@@ -139,11 +158,11 @@ $.extend(HeartbeatCanvas.prototype, {
 		if (this._pulseInterval) {
 			window.clearTimeout(this._pulseInterval);
 		}
-		this.beat(this.beatMax, this.beatSpeed);
+		this.beat(this.visibility, this.beatSpeed);
 		var soundFile = this._pulseToSoundFile(this.pulse);
 		this.lastClone = (this.lastClone + 1) % 5;
 		lowLag.play(soundFile, this.lastClone);
-		window.setTimeout(function() {self.beat(self.beatMax, self.beatSpeed);}, this._soundFileToDelay(soundFile));
+		window.setTimeout(function() {self.beat(self.visibility, self.beatSpeed);}, this._soundFileToDelay(soundFile));
 		this._pulseInterval = window.setTimeout($.proxy(this,'_pulseBeat'), 60000.0 / this.pulse);
 	},
 	beat: function(maxRadius, speed) {
@@ -252,9 +271,9 @@ $.extend(HeartbeatCanvas.prototype, {
 			ctx.closePath();
 			ctx.stroke();
 
-			ctx.lineWidth = 2;
-			ctx.strokeStyle = 'rgba(255,255,255,0.9)';
-			var arcWidth = Math.PI / 3 * beat.opacity;
+			ctx.lineWidth = 3;
+			ctx.strokeStyle = 'rgba(255,255,255,0.7)';
+			var arcWidth = Math.PI / 4 * beat.opacity;
 			ctx.beginPath();
 			ctx.arc(800, 600, beat.radius, this._homeDirection - arcWidth, this._homeDirection + arcWidth);
 			ctx.stroke();
